10cff7dc0b56e391d2c7840efdbf2b64e72470b7
Simou <wassim.chaguetmi@gmail.com>
2022-07-03 11:46:27 +0100

    fixed registration

--- app/src/main/java/fr/soumiabrk/patientapp/Register.kt
+++ app/src/main/java/fr/soumiabrk/patientapp/Register.kt
@@ -15,7 +15,8 @@ import java.util.*
 
 class Register : AppCompatActivity() {
     lateinit var username: EditText
-     var gender = " "
+    var gender = ""
+    var selectedDate = ""
 
     lateinit var firstName: EditText
     lateinit var LastName: EditText
@@ -48,7 +49,7 @@ class Register : AppCompatActivity() {
         cancel = findViewById(R.id.register_button_cancel)
         username = findViewById(R.id.username)
         firstName = findViewById(R.id.firstName)
-        LastName =  findViewById(R.id.LastName)
+        LastName = findViewById(R.id.LastName)
         phone = findViewById(R.id.phone)
         address = findViewById(R.id.address)
         mydate = findViewById(R.id.myDate)
@@ -60,57 +61,15 @@ class Register : AppCompatActivity() {
         radioGender = findViewById(R.id.radioGender)
         radioGender.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
-                R.id.radioMale -> {gender == "M"
-                }
-                R.id.radioFemale -> {gender == "F" }
+                R.id.radioMale -> gender = "M"
+                R.id.radioFemale -> gender = "F"
             }
         }
 
 
 
 
-        reg.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                val Mail: String
-                val passw: String
-                val Cpassw: String
-                val username: String
-                val firstName: String
-                val LastName: String
-                val phone: String
-                val address: String
-
-                Mail = email.getText().toString()
-                passw = pass.getText().toString()
-                Cpassw = ConfirmPass.getText().toString()
-                username = this@Register.username.getText().toString()
-                firstName = this@Register.firstName.getText().toString()
-                LastName = this@Register.LastName.getText().toString()
-                phone = this@Register.phone.getText().toString()
-                address = this@Register.address.getText().toString()
-
-                if (username == "");
-                run { Toast.makeText(this@Register, "Nom utilisateur est vide", Toast.LENGTH_SHORT).show() }
-                if (firstName == "");
-                run { Toast.makeText(this@Register, "Prénom est vide", Toast.LENGTH_SHORT).show() }
-                if (LastName == "");
-                run { Toast.makeText(this@Register, "Nom est vide", Toast.LENGTH_SHORT).show() }
-                if (phone == "");
-                run { Toast.makeText(this@Register, "Numéro de téléphone est vide", Toast.LENGTH_SHORT).show() }
-                if (address == "");
-                run { Toast.makeText(this@Register, "Adresse est vide", Toast.LENGTH_SHORT).show() }
-                if (Mail == "");
-                run { Toast.makeText(this@Register, "Email est vide", Toast.LENGTH_SHORT).show() }
-                if (passw == "");
-                run { Toast.makeText(this@Register, "mot de passe est vide", Toast.LENGTH_SHORT).show() }
-                if (Cpassw == "");
-                run { Toast.makeText(this@Register, "mot de passe est vide", Toast.LENGTH_SHORT).show() }
-                if ((Mail == "") and (passw == "") and (Cpassw == ""));
-                run { Toast.makeText(this@Register, "les informations sont vide", Toast.LENGTH_SHORT).show() }
-
-                register()
-            }
-        })
+        reg.setOnClickListener { register() }
 
         Ibtn.setOnClickListener(View.OnClickListener {
             val R = Intent(this@Register, Login::class.java)
@@ -120,12 +79,14 @@ class Register : AppCompatActivity() {
         )
         val datePicker = findViewById<DatePicker>(R.id.datePicker)
         val today = Calendar.getInstance()
-        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+        datePicker.init(
+            today.get(Calendar.YEAR), today.get(Calendar.MONTH),
             today.get(Calendar.DAY_OF_MONTH)
 
         ) { view, year, month, day ->
-            val month = month + 1
-            val msg = "You Selected: $day/$month/$year"
+            val mMonth = month + 1
+            selectedDate = "$year-$mMonth-$day"
+            val msg = "You Selected: $day/$mMonth/$year"
             mydate!!.text = msg
             Toast.makeText(this@Register, msg, Toast.LENGTH_SHORT).show()
         }
@@ -139,11 +100,11 @@ class Register : AppCompatActivity() {
             "password" to pass.text.toString(),
             "first_name" to firstName.text.toString(),
             "last_name" to LastName.text.toString(),
-            "account" to mapOf<String, Any?>(
+            "account" to mapOf(
                 "user_type" to "P",
-                "birthday" to mydate,
+                "birthday" to selectedDate,
                 "phone_num" to phone.text.toString(),
-                "address" to  address.text.toString(),
+                "address" to address.text.toString(),
                 "gender" to gender,
             ),
         )
